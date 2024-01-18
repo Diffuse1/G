d0c370ea7049997b107d355313e7e3bb3bbf3306
Juan Pablo Resendez Rosas <resendes.rosas614@gmail.com>
2024-01-18 08:50:51 -0600

    Prueba

---
+++ lib/classes.py
@@ -0,0 +1,10 @@
+class Rectangulo:
+    def __int__(self,largo,ancho):
+        self.largo=largo
+        self.ancho=ancho
+        pass
+    def __str__(self):
+        return f"Rectangulo de largo {self.largo} y ancho{self.ancho}"
+    
+    
+    
